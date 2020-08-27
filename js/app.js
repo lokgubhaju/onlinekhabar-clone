f5449f1dc0e1c736fcb72af24b420b592fd0d89a
Lok Gubhaju <lok.gubhaju@gmail.com>
2020-08-27 12:24:13 +0200

    code optimized

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function () {
+  myFunction();
+};
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
   }
